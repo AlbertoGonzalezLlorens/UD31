0372ca126055356fe1a876ab19bb40e57f603f96
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-12-04 13:13:09 +0100

    Update codigo.js

--- 12/codigo.js
+++ 12/codigo.js
@@ -1,5 +1,5 @@
 function validarFecha(fecha) {
-    var pattern = /([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])(\/|-)([0-2][0-9][0-9][0-9])/ ;
+    var pattern = /(0?[1-9]|[12][1-9]|3[01])\/(0?[1-9]|1[0-2])\/([12][0-9])\d(2)/;
     console.log(pattern.test("Nací el " + fecha + " en Donostia."));
 }
 validarFecha("05/04/1982");
